fde3c706a997689e414ce5cd38e4047e59bf6aa5
Chleen <cl27au@gmail.com>
2021-07-13 10:03:23 +0900

    if study 1

--- src/com/cl/s1/Test8.java
+++ src/com/cl/s1/Test8.java
@@ -27,6 +27,8 @@ public class Test8 {
 		// 평균이 60점 이상
 		// 과목당 40점 이상
 		
+/* 		
+ 		//내 풀이		
 		if((kor < 40 || eng < 40 || math < 40) || avg < 60) {
 			System.out.println("불합격");			
 		}else {
@@ -34,6 +36,61 @@ public class Test8 {
 		}
 				
 		System.out.println("Test8 Finish");
+*/
+		
+//		--------------------------3차 코드------------------------------
+		if(avg>=60 && kor>40 && eng>40 && math>40) {
+			System.out.println("합격");
+		}else {
+			System.out.println("불합격");
+		}
+		
+//		--------------------------3차 코드------------------------------
+		
+		
+/*		
+		--------------------------2차 코드------------------------------
+		if (avg >= 60) {
+			if(kor>40 && eng>40 && math>40) {
+				System.out.println("합격");
+			}else {
+				System.out.println("불합격");
+			}
+		}else {
+			System.out.println("불합격");
+		}
+		--------------------------2차 코드------------------------------
+		
+		
+	
+  		--------------------------1차 코드------------------------------
+		if(avg >= 60) {
+			if(kor>40) {
+				if(eng > 40) {
+					if(math > 40) {
+						System.out.println("합격");
+					}else {
+						System.out.println("수학 과락 불합격");
+					}
+					System.out.println("합격");
+				}else {
+					System.out.println("영어 과락 불합격");
+				}
+				System.out.println("합격");
+			}else {
+				System.out.println("국어 과락 불합격");
+			}
+			System.out.println("합격");
+		}else {
+			System.out.println("불합격");
+		}
+		--------------------------1차 코드------------------------------
+*/
+		
+		//단축키
+		//ctrl + Shift + / : 선택한 블럭 전체 주석처리 (해제 : ctrl + Shift + \)
+		//ctrl + / : 단일주석, 해제
+		
 		
 	}
 	
