c17d374d3599bde1609991c854a5c8fd7a251a06
Chleen <cl27au@gmail.com>
2021-07-19 09:34:19 +0900

    Update Array_8.java

--- src/com/cl/s4/Array_8.java
+++ src/com/cl/s4/Array_8.java
@@ -14,8 +14,10 @@ public class Array_8 {
 		int id = 0;
 		int pw = 0;
 		boolean flag = true;
-		int [] tmpIds = new int [ids.length + 1];
-		int [] tmpPws = new int [pws.length + 1];
+		boolean loginCheck = false;
+		
+		//탈퇴회원 인덱스
+		int delNum = 0; 
 		
 		//프로그램 시작
 		while(flag) {
@@ -39,9 +41,13 @@ public class Array_8 {
 					if(yId == ids[i] && yPw == pws[i]) {
 						login = "로그인 성공";
 						flag = false;
+						loginCheck = true;
+						delNum = i;
 						break;
 					}
 				}
+				
+				
 				
 				System.out.println(login);
 				break;
@@ -51,6 +57,8 @@ public class Array_8 {
 			case 2:
 				
 				boolean check = true;
+				int [] tmpIds = new int [ids.length + 1];
+				int [] tmpPws = new int [pws.length + 1];
 				
 				System.out.println("회원가입");
 				System.out.println("Id를 설정하세요");
@@ -65,7 +73,7 @@ public class Array_8 {
 				}
 			
 				
-				while(check) {
+				if(check==true) {
 					System.out.println("Pw를 설정하세요");
 					int newPw = sc.nextInt();
 					
@@ -87,10 +95,7 @@ public class Array_8 {
 					pws = tmpPws;
 					
 					check = false;
-				}//while
-					
-				
-				
+				}
 				break;
 				
 			//종료
@@ -98,8 +103,68 @@ public class Array_8 {
 				System.out.println("종료합니다");
 				flag = false;
 			}
+		
+			//========================================================
+			//1번 로그인 성공하면 메인메뉴
+			//1. 로그아웃
+			//2. 회원탈퇴
+			//3. 종료
+			if(loginCheck==true) {
+				System.out.println("1. 로그아웃  2. 회원탈퇴  3. 종료");
+				int select2 = sc.nextInt();
 				
-		}
+				switch(select2) {
+				
+				//로그아웃
+				case 1:
+					System.out.println("로그아웃 되셨습니다");
+					flag = true;
+					break;
+				
+				//회원탈퇴
+				case 2:
+					int [] delIds = new int[ids.length -1];
+					int [] delPws = new int[pws.length -1];
+					int idx = 0;
+					//delNum : 탈퇴하는 사람 인덱스 번호
+					
+					//아이디 삭제
+					for(int i=0; i<ids.length; i++) {
+						if(delNum == i) {
+							continue;
+						}
+						delIds[idx] = ids[i];
+						idx++;
+					}
+					ids = delIds;
+					
+					//비밀번호 삭제
+					for(int i=0; i<pws.length; i++) {
+						if(delNum == i) {
+							continue;
+						}
+						delPws[idx] = pws[i];
+						idx++;
+					}
+					pws = delPws;
+					
+					for(int i=0; i<ids.length; i++) {
+						System.out.println(ids[i]);
+					}
+				
+					
+					break;
+					
+				//종료	
+				default:
+				}
+			}
+		}//while 끝
+		
+		
+		
+		
+	
 		
 		
 		
